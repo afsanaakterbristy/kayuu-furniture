d0e2f0c197bf739012e4050bcab794d14d779b1c
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-11-25 16:05:04 +0600

    added

--- src/Dashboard/Payment.js
+++ src/Dashboard/Payment.js
@@ -1,17 +1,22 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData, useNavigation } from 'react-router-dom';
 import Loading from '../components/Shareds/Loading';
+import CheckoutForm from './CheckoutForm';
 
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 const Payment = () => {
     const booking =useLoaderData();
     const navigation = useNavigation();
-    const { name, resaleprice } = booking;
+    const { productsname, resaleprice } = booking;
     if(navigation.state === "loading"){
         return <Loading></Loading>
     }
     return (
         <div>
-            <h3 className="text-3xl">Payment for {name}</h3>
+            <h3 className="text-3xl">Payment{productsname}</h3>
             <p className="text-xl">Please pay ${resaleprice}</p>
             <div className='w-96 my-12'>
                 <Elements stripe={stripePromise}>
